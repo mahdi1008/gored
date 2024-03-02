bc9bdedcb77f23077ce67c99e22f45ff78393621
mahdi <mahdi@mahdis-MacBook-Pro.local>
2024-03-03 01:30:45 +0330

    Add Array type

--- respser/respser.go
+++ respser/respser.go
@@ -5,22 +5,57 @@ import (
 	"strconv"
 )
 
-func EncodeSimpleString(s string) string {
-	return fmt.Sprintf("+%s\r\n", s)
+type RespEncoder interface {
+	RespEncode() string
 }
 
-func EncodeErrorString(s string) string {
-	return fmt.Sprintf("-%s\r\n", s)
+type SimpleString struct {
+	S string
 }
 
-func EncodeInteger(n int) string {
-	nStr := strconv.Itoa(n)
+func (ss *SimpleString) RespEncode() string {
+	return fmt.Sprintf("+%s\r\n", ss.S)
+}
+
+type ErrorString struct {
+	E string
+}
+
+func (es *ErrorString) RespEncode() string {
+	return fmt.Sprintf("-%s\r\n", es.E)
+}
+
+type Integer struct {
+	N int
+}
+
+func (i *Integer) RespEncode() string {
+	nStr := strconv.Itoa(i.N)
 	return fmt.Sprintf(":%s\r\n", nStr)
 }
 
-func EncodeBulkStrings(s *string) string {
-	if s == nil {
+type BulkString struct {
+	S *string
+}
+
+func (bs *BulkString) RespEncode() string {
+	if bs.S == nil {
 		return "$-1\r\n"
 	}
-	return fmt.Sprintf("$%d\r\n%s\r\n", len(*s), *s)
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(*bs.S), *bs.S)
+}
+
+type Array struct {
+	Elements *[]RespEncoder
+}
+
+func (a *Array) RespEncode() string {
+	if a.Elements == nil {
+		return "*-1\r\n"
+	}
+	res := fmt.Sprintf("*%d\r\n", len(*a.Elements))
+	for _, e := range *a.Elements {
+		res = res + e.RespEncode()
+	}
+	return res
 }
--- respser/respser_test.go
+++ respser/respser_test.go
@@ -7,16 +7,16 @@ import (
 func TestEncodeSimpleString(t *testing.T) {
 	testCases := []struct {
 		name  string
-		input string
+		input SimpleString
 		want  string
 	}{
-		{"normal_string", "Hello, World!", "+Hello, World!\r\n"},
-		{"integer_string", "123", "+123\r\n"},
+		{"normal_string", SimpleString{S: "Hello, World!"}, "+Hello, World!\r\n"},
+		{"integer_string", SimpleString{S: "123"}, "+123\r\n"},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := EncodeSimpleString(tc.input)
+			res := tc.input.RespEncode()
 			if tc.want != res {
 				t.Errorf("Expected: %s, Got: %s", tc.want, res)
 			}
@@ -27,16 +27,16 @@ func TestEncodeSimpleString(t *testing.T) {
 func TestEncodeErrorString(t *testing.T) {
 	testCases := []struct {
 		name  string
-		input string
+		input ErrorString
 		want  string
 	}{
-		{"normal_string", "Hello, World!", "-Hello, World!\r\n"},
-		{"integer_string", "123", "-123\r\n"},
+		{"normal_string", ErrorString{E: "Hello, World!"}, "-Hello, World!\r\n"},
+		{"integer_string", ErrorString{E: "123"}, "-123\r\n"},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := EncodeErrorString(tc.input)
+			res := tc.input.RespEncode()
 			if tc.want != res {
 				t.Errorf("Expected: %s, Got: %s", tc.want, res)
 			}
@@ -47,17 +47,17 @@ func TestEncodeErrorString(t *testing.T) {
 func TestEncodeInteger(t *testing.T) {
 	testCases := []struct {
 		name  string
-		input int
+		input Integer
 		want  string
 	}{
-		{"encode_positive_integer", 1000, ":1000\r\n"},
-		{"encode_zero", 0, ":0\r\n"},
-		{"encode_negative_integer", -3, ":-3\r\n"},
+		{"encode_positive_integer", Integer{N: 1000}, ":1000\r\n"},
+		{"encode_zero", Integer{N: 0}, ":0\r\n"},
+		{"encode_negative_integer", Integer{N: -3}, ":-3\r\n"},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := EncodeInteger(tc.input)
+			res := tc.input.RespEncode()
 			if res != tc.want {
 				t.Errorf("Expected: %s, Got: %s", tc.want, res)
 			}
@@ -65,25 +65,69 @@ func TestEncodeInteger(t *testing.T) {
 	}
 }
 
+func stringPointer(s string) *string {
+	return &s
+}
+
 func TestEncodeBulkStrings(t *testing.T) {
 	testCases := []struct {
-		name       string
-		bulkString string
-		expectNil  bool
-		want       string
+		name  string
+		input BulkString
+		want  string
 	}{
-		{"non-empty_string", "hello", false, "$5\r\nhello\r\n"},
-		{"empty_string", "", false, "$0\r\n\r\n"},
-		{"nil_string", "", true, "$-1\r\n"}, // Here we use an empty string to represent a nil bulkString.
+		{"encode_non_empty_string", BulkString{S: stringPointer("hello")}, "$5\r\nhello\r\n"},
+		{"encode_empty_string", BulkString{S: stringPointer("")}, "$0\r\n\r\n"},
+		{"encode_nil_string", BulkString{}, "$-1\r\n"},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var bs *string
-			if !tc.expectNil {
-				bs = &tc.bulkString
+			res := tc.input.RespEncode()
+			if res != tc.want {
+				t.Errorf("Expected: %s, Got: %s", tc.want, res)
 			}
-			res := EncodeBulkStrings(bs)
+		})
+	}
+}
+
+func TestEncodeArray(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input *Array
+		want  string
+	}{
+		{
+			"encode_normal_array",
+			&Array{Elements: &[]RespEncoder{&Integer{N: 1}, &Integer{N: 2}, &Integer{N: 3}, &Integer{N: 4}, &BulkString{S: stringPointer("hello")}}},
+			"*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n",
+		},
+		{
+			"encode_empty_array",
+			&Array{Elements: &[]RespEncoder{}},
+			"*0\r\n",
+		},
+		{
+			"encode_nil_array",
+			&Array{},
+			"*-1\r\n",
+		},
+		{
+			"encode_nested_array",
+			&Array{Elements: &[]RespEncoder{
+				&Array{
+					Elements: &[]RespEncoder{&Integer{N: 1}, &Integer{N: 2}, &Integer{N: 3}},
+				},
+				&Array{
+					Elements: &[]RespEncoder{&SimpleString{S: "Hello"}, &ErrorString{"World"}},
+				},
+			}},
+			"*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := tc.input.RespEncode()
 			if res != tc.want {
 				t.Errorf("Expected: %s, Got: %s", tc.want, res)
 			}
