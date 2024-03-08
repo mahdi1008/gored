b2b534b206f3d66035752227548978a92e853563
mahdi <mahdi@mahdis-MacBook-Pro.local>
2024-03-08 18:07:58 +0330

    Add PING command functionality

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+	"gored/respser"
 	"net"
 )
 
 func main() {
-	// Listen on TCP port 6380 on all interfaces.
 	listener, err := net.Listen("tcp", ":6380")
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
@@ -16,45 +16,84 @@ func main() {
 	fmt.Println("Listening on :6380")
 
 	for {
-		// Wait for a connection.
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
 			continue
 		}
-		// Handle connections in a new goroutine.
 		go handleRequest(conn)
 	}
 }
 
-// Handles incoming requests.
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
 
 	fmt.Println("handling response")
 	for {
-		buffer := make([]byte, 1024)
+		buffer := make([]byte, 4096)
 		length, err := conn.Read(buffer)
 		if err != nil {
 			fmt.Println("Error reading:", err.Error())
 			return
 		}
-		fmt.Println(string(buffer[:length]))
+		in := string(buffer[:length])
 
-		// This is where you can customize the response.
-		// For example, sending "resp" as a byte array.
+		re, err := respser.RespDecode(in)
+		response := ""
+		if err != nil {
+			fmt.Println("Error decoding:", err)
+		} else {
+			switch v := re.(type) {
+			case *respser.SimpleString:
+				fmt.Println("SimpleString:", v)
+			case *respser.ErrorString:
+				fmt.Println("ErrorString:", v)
+			case *respser.Integer:
+				fmt.Println("Integer:", v)
+			case *respser.BulkString:
+				fmt.Println("BulkString:", v)
+			case *respser.Array:
+				a := re.(*respser.Array)
+				for _, e := range *a.Elements {
+					switch v2 := e.(type) {
+					case *respser.SimpleString:
+						fmt.Println("SimpleString:", *v2)
+					case *respser.ErrorString:
+						fmt.Println("ErrorString:", *v2)
+					case *respser.Integer:
+						fmt.Println("Integer:", *v2)
+					case *respser.BulkString:
+						fmt.Println("BulkString:", *v2.S)
+						response = handleCommand(*v2.S)
+					case *respser.Array:
+						fmt.Println("BulkString:", *v2)
+					default:
+						fmt.Println("Unknown type internal")
+					}
+					fmt.Println("Array:", v)
+				}
+			default:
+				fmt.Println("Unknown type")
+			}
+		}
 
-		// command := string(buffer[:length])
-
-		_, err = conn.Write([]byte("+PONG\r\n"))
+		if response == "" {
+			response = "+OK\r\n"
+		}
+		_, err = conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Error writing:", err.Error())
 		}
-		// Close the connection when you're done with it.
-
 		if err != nil {
 			fmt.Println("Error sending response: ", err.Error())
 		}
-		// time.Sleep(time.Second * 1)
 	}
+}
+
+func handleCommand(s string) string {
+	if s == "PING" {
+		ss := respser.SimpleString{S: "PONG"}
+		return ss.RespEncode()
+	}
+	return ""
 }
